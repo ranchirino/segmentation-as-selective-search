0cd8472f96a93ebdb7d19c93b2c772b92ba8f862
ranchirino <ranchirino@gmail.com>
2018-11-06 07:16:32 -0600

    Update selective_search.py

--- selective_search.py
+++ selective_search.py
@@ -5,6 +5,7 @@ from matplotlib.patches import Rectangle
 from skimage import segmentation
 from skimage import measure
 from skimage.data import coffee
+from datetime import datetime
 # import selectivesearch
 
 img = coffee()
@@ -23,9 +24,8 @@ init_segments = segmentation.felzenszwalb(img, scale=10, sigma=0.8, min_size=100
 # %% extract regions
 
 def extract_regions(init_segments):
-    R = {}  # x_min, y_min, width, height, label
-
-    for r in range(init_segments.max()+1):
+    R = []
+    for r in np.unique(init_segments):
         i = np.asarray(np.where(init_segments == r))
         x_min = i[1,:].min()
         x_max = i[1,:].max()
@@ -33,53 +33,69 @@ def extract_regions(init_segments):
         y_max = i[0,:].max()
         width = (x_max - x_min) + 1
         height = (y_max - y_min) + 1
-        R[r] = {"x_min": x_min, "y_min": y_min, "width": width, "height": height, "label": r}
-
+        R.append({"x_min": x_min, "x_max": x_max, "y_min": y_min, "y_max": y_max, "width": width, "height": height, "size": i.shape[1], "label": r})
     return R
 
 R = extract_regions(init_segments)
 
-plt.figure()
-plt.imshow(init_segments)
-for r in R.items():
-    d = r[1]
-    rect = Rectangle((d['x_min'], d['y_min']), d['width'], d['height'], fill=False, color='red', linewidth=1.0)
-    plt.axes().add_patch(rect)
-plt.show()
+# plt.figure()
+# plt.imshow(init_segments)
+# for r in R.items():
+#     d = r[1]
+#     rect = Rectangle((d['x_min'], d['y_min']), d['width'], d['height'], fill=False, color='red', linewidth=1.0)
+#     plt.axes().add_patch(rect)
+# plt.show()
 
 # plt.imshow(init_segments)
 # plt.show()
 
 # %% extract neighbors
 
-# def extract_neighbors(regions):
+def find_neighbours(reg_bb, label):
+    reg = extract_regions(reg_bb)
+    neig = []
+    for r in reg:
+        if r["label"] != label:
+            window = reg_bb[r["y_min"]:r["y_max"]+1, r["x_min"]:r["x_max"]+1]
+            if label in window:
+                neig.append(r["label"])
+    return neig
 
-zeros_arr = init_segments[R[0]["x_min"]:R[0]["height"]+1, R[0]["y_min"]:R[0]["width"]+1]
+def extract_neighbors(seg_img, regions, img):
+    N = []  # region, neighbours
+    h = img.shape[0]  # rows
+    w = img.shape[1]  # columns
+    for r in regions:
+        x_min = r["x_min"] - 1 if r["x_min"] != 0 else r["x_min"] # +1 padding
+        x_max = r["x_max"] + 2 if r["x_max"] != w else r["x_max"] # +1 padding
+        y_min = r["y_min"] - 1 if r["y_min"] != 0 else r["y_min"] # +1 padding
+        y_max = r["y_max"] + 2 if r["y_max"] != h else r["y_max"] # +1 padding
+        reg_bb = seg_img[y_min:y_max, x_min:x_max]
+        neig = find_neighbours(reg_bb, r["label"])
+        N.append({"region": r["label"], "neig": neig})
+    return N
+
+# t1 = datetime.now()
+N = extract_neighbors(init_segments, R, img)
+# t2 = datetime.now()
+# print(t2-t1)
+
+# bb_14 = init_segments[R[14]["y_min"]-1:R[14]["y_max"]+2, R[14]["x_min"]-1:R[14]["x_max"]+2]
 # zeros_arr[np.where(zeros_arr != 0)] = 1
-plt.imshow(zeros_arr)
-plt.show()
-np.unique(zeros_arr[np.where(zeros_arr != 0)])
+# plt.imshow(zeros_arr)
+# plt.show()
+# np.unique(zeros_arr[np.where(zeros_arr != 0)])
 
-contours = measure.find_contours(zeros_arr, 0, fully_connected='high')
-# cont = np.concatenate(contours)
-cont = np.round(np.concatenate(contours)).astype('int32')
+# contours = measure.find_contours(zeros_arr, 0)
+# cont = np.round(np.concatenate(contours)).astype('int32')
 # plt.imshow(contours)
 # plt.show()
 
-# c1 = np.round(contours[0]).astype('int32')
-# c2 = np.round(contours[1]).astype('int32')
-# c3 = np.round(contours[2]).astype('int32')
-# c4 = np.round(contours[3]).astype('int32')
-# c5 = np.round(contours[4]).astype('int32')
-zeros_arr[cont[:,0], cont[:,1]] = 20
-# zeros_arr[c2[:,0], c2[:,1]] = 20
-# zeros_arr[c3[:,0], c3[:,1]] = 20
-# zeros_arr[c4[:,0], c4[:,1]] = 20
-# zeros_arr[c5[:,0], c5[:,1]] = 20
+# zeros_arr[cont[:,0], cont[:,1]] = 20
 # props = measure.regionprops(zeros_arr)
 # zeros_arr[props[0].coords[:,0], props[0].coords[:,1]] = 20
-plt.imshow(zeros_arr)
-plt.show()
+# plt.imshow(zeros_arr)
+# plt.show()
 
 # n = []
 # for cont in contours:
@@ -90,4 +106,17 @@ plt.show()
 #     if 0 in reg:
 #         n.append(reg[(reg != 20) & (reg != 0)])
 
-# hacer un bounding box alrededor de cada region y ver si en ese bb se encuentra la region que estoy buscando, eso me dice si son vecinos o no
+
+#%% calculate similarity
+
+def calc_sim(r1, r2, img):
+    img_size = img.shape[0] * img.shape[1]
+    r1 = [x for x in R if x['label'] == r1]
+    r2 = [x for x in R if x['label'] == r2]
+
+
+
+# def sim_size():
+    # calculate the size similarity over the image
+
+[x for x in R if x['label'] == 13]
